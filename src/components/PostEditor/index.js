0aebb402b115fd5252ae18ea3a026d8ddb4d58c5
Bruno Correia <brunocs@msn.com>
2018-07-01 13:08:14 -0300

    Improve post editor behavior

--- src/components/PostEditor/index.js
+++ src/components/PostEditor/index.js
@@ -8,16 +8,36 @@ import PostEditorFooter from '../PostEditorFooter';
 import Avatar1 from '../../assets/images/avatars/01.png';
 
 class PostEditor extends Component {
-  state = {};
+  state = {
+    content: '',
+    placeholder: 'No que você está pensando?',
+    editing: false,
+  };
+
+  handleEditorFocus = () => {
+    this.setState({ editing: true });
+  };
+
+  handleEditorBlur = () => {
+    const content = document.getElementById('textarea').textContent;
+    this.setState({ content, editing: false });
+  };
 
   render() {
+    const { content, placeholder, editing } = this.state;
     return (
       <Container>
-        <Grid style={{ display: 'flex' }}>
+        <Grid>
           <Avatar src={Avatar1} alt="" />
 
-          <TextArea contentEditable="true">
-No que você está pensando?
+          <TextArea
+            contentEditable="true"
+            onFocus={this.handleEditorFocus}
+            onBlur={this.handleEditorBlur}
+            suppressContentEditableWarning
+            id="textarea"
+          >
+            {!editing && content.length < 1 ? placeholder : content}
           </TextArea>
         </Grid>
         <PostEditorFooter />
