16487f490fe3d87914a90c41c0857c5bf1319b1a
Bruno Correia <brunocs@msn.com>
2018-07-16 00:04:58 -0300

    Connect GuestHeader component to redux

--- src/components/GuestHeader/index.js
+++ src/components/GuestHeader/index.js
@@ -1,4 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as UserActions } from '../../store/ducks/users';
+
 import {
   Container,
   Grid,
@@ -11,36 +16,54 @@ import {
   Button,
 } from './styles';
 
-const Header = () => (
-  <Container>
-    <Grid>
-      <GridColumn>
-        <Title>
+class GuestHeader extends Component {
+  handleSubmit() {
+    const { signup } = this.props;
+    return signup();
+  }
+
+  render() {
+    return (
+      <Container>
+        <Grid>
+          <GridColumn>
+            <Title>
 Postsbook
-        </Title>
-      </GridColumn>
-      <GridColumn padding="6px 0 0 48px">
-        <Form>
-          <FormField padding="0 0 0 14px">
-            <Label htmlFor="email">
+            </Title>
+          </GridColumn>
+          <GridColumn padding="6px 0 0 48px">
+            <Form>
+              <FormField padding="0 0 0 14px">
+                <Label htmlFor="email">
 Email
-            </Label>
+                </Label>
 
-            <Input type="text" name="email" margin="4px 0 0 0" />
-          </FormField>
-          <FormField padding="0 0 0 14px">
-            <Label htmlFor="password">
+                <Input type="text" name="email" margin="4px 0 0 0" />
+              </FormField>
+              <FormField padding="0 0 0 14px">
+                <Label htmlFor="password">
 Senha
-            </Label>
-            <Input type="password" name="password" margin="4px 0 0 0" />
-          </FormField>
-          <Button type="submit">
-Entrar
-          </Button>
-        </Form>
-      </GridColumn>
-    </Grid>
-  </Container>
-);
+                </Label>
+                <Input type="password" name="password" margin="4px 0 0 0" />
+              </FormField>
+              <Button type="submit" onClick={this.handleSubmit}>
+                Entrar
+              </Button>
+            </Form>
+          </GridColumn>
+        </Grid>
+      </Container>
+    );
+  }
+}
 
-export default Header;
+const mapStateToProps = state => ({
+  user: state.user,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(UserActions, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(GuestHeader);
