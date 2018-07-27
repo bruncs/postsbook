5c5678aaa64f73ba7b2a93aa74857d1258dc2880
Bruno Correia <brunocs@msn.com>
2018-07-27 03:46:43 -0300

    Improvements on header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,62 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Container, Title } from './styles';
 import { Creators as UserCreators } from '../../store/ducks/user';
+import {
+  Container, Grid, GridColumn, Button, ProfileButton, Divider, Avatar,
+} from './styles';
+import Logo from '../../assets/images/icons/whitelogo.png';
+import Avatar1 from '../../assets/images/avatars/01.png';
 
 class Header extends Component {
-  handleClick = () => {
-    this.props.signout();
+  static propTypes = {
+    signoutRequest: PropTypes.func.isRequired,
+    user: PropTypes.shape({
+      isAuthenticated: PropTypes.bool.isRequired,
+    }).isRequired,
+  };
+
+  state = {};
+
+  handleSignout = () => {
+    const { signoutRequest } = this.props;
+    signoutRequest();
   };
 
   render() {
-    if (!this.props.user.isAuthenticated) {
-      return <Redirect to="./" />;
-    }
+    const { user } = this.props;
+    const firstName = user.data.user.name.split(' ')[0];
     return (
       <Container>
-        <Title>
-PostsBook
-        </Title>
-        <button type="button" onClick={this.handleClick}>
-          Sair
-        </button>
+        <Grid>
+          <GridColumn justifyContent="flex-start" alignItems="center" padding="4px 0 0 0">
+            <Link to="./feed">
+              <img alt="" src={Logo} />
+            </Link>
+          </GridColumn>
+          <GridColumn justifyContent="flex-end" alignItems="center">
+            <Link to={`./profile?id=${user.data.user._id}`} style={{ textDecoration: 'none' }}>
+              <ProfileButton>
+                <Avatar alt="" src={Avatar1} />
+                <Divider />
+                {firstName}
+              </ProfileButton>
+            </Link>
+            <Divider />
+            <Link to="./feed">
+              <Button name="feed" type="button">
+                Página Inicial
+              </Button>
+            </Link>
+            <Divider />
+            <Button name="signout" type="button" onClick={this.handleSignout}>
+              Sair
+            </Button>
+          </GridColumn>
+        </Grid>
       </Container>
     );
   }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -12,8 +12,79 @@ export const Container = styled.div`
   top: 0;
 `;
 
-export const Title = styled.h1`
-  color: #fff;
-  font-size: 20px;
+export const Grid = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 1000px;
+`;
+
+export const GridColumn = styled.div`
+  width: 450px;
+  display: flex;
+  justify-content: ${(props) => {
+    const { justifyContent } = props;
+    return justifyContent || 'space-around';
+  }};
+  align-items: ${(props) => {
+    const { alignItems } = props;
+    return alignItems || 'center';
+  }};
+  padding: ${(props) => {
+    const { padding } = props;
+    return padding || '0px';
+  }};
+`;
+
+export const Button = styled.button`
+  height: 28px;
+  background: ${(props) => {
+    const { background } = props;
+    return background || '#4267b2';
+  }};
+  padding: ${(props) => {
+    const { padding } = props;
+    return padding || '0px 10px 0 10px';
+  }};
+  background-position: 2px 2px;
+  background-repeat: no-repeat;
+  background-size: 24px 24px;
+  border: none;
+  border-radius: 2px;
+  font-size: 12px;
   font-weight: bold;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3b5ca0;
+  }
+`;
+
+export const ProfileButton = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 28px;
+  padding: 0px 10px 0 10px;
+  border: none;
+  border-radius: 2px;
+  font-size: 12px;
+  font-weight: bold;
+  color: #fff;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #3b5ca0;
+  }
+`;
+
+export const Divider = styled.div`
+  width: 5px;
+`;
+
+export const Avatar = styled.img`
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  margin: 0 5px 0 0;
 `;
