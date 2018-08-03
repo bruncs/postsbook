0818da17d35c7ceeeb17ee11cd3e1e4ed38bd3cc
Bruno Correia <brunocs@msn.com>
2018-08-02 23:59:33 -0300

    Add auth control to guest page

--- src/pages/guest/index.js
+++ src/pages/guest/index.js
@@ -1,12 +1,34 @@
 import React, { Component, Fragment } from 'react';
+import { Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Creators as UserCreators } from '../../store/ducks/user';
 
 import GuestHeader from '../../components/GuestHeader';
 import GuestContent from '../../components/GuestContent';
 
 class Guest extends Component {
+  static propTypes = {
+    user: PropTypes.shape({
+      isAuthenticated: PropTypes.bool.isRequired,
+    }),
+  };
+
+  static defaultProps = {
+    user: {
+      isAuthenticated: false,
+    },
+  };
+
   state = {};
 
   render() {
+    const { user } = this.props;
+    if (user.isAuthenticated) {
+      return <Redirect to="./feed" />;
+    }
     return (
       <Fragment>
         <GuestHeader />
@@ -16,4 +38,13 @@ class Guest extends Component {
   }
 }
 
-export default Guest;
+const mapStateToProps = state => ({
+  user: state.user,
+});
+
+const mapDispatchToProps = dispatch => bindActionCreators(UserCreators, dispatch);
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(Guest);
