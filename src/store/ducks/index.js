e181c4a9e99fc0a76c467cfb675e09965691a0e6
Bruno Correia <brunocs@msn.com>
2018-09-30 16:35:00 -0300

    Add store cleanup at logout

--- src/store/ducks/index.js
+++ src/store/ducks/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
 
-import user from './user';
+import user, { Types as UserTypes } from './user';
 import feed from './feed';
 import profile from './profile';
 import friendship from './friendship';
@@ -9,7 +9,7 @@ import like from './like';
 
 // TODO: Criar duck + saga de error handling
 
-export default combineReducers({
+const appReducer = combineReducers({
   user,
   feed,
   profile,
@@ -17,3 +17,23 @@ export default combineReducers({
   post,
   like,
 });
+
+const rootReducer = (state, action) => {
+  let currentState = state;
+
+  // Em caso de logout ou solicitação de registro, limpa o estado e o localStorage
+  if (action.type === UserTypes.SIGNOUT_SUCCESS) {
+    try {
+      Object.keys(state).forEach((key) => {
+        localStorage.removeItem(`persist:${key}`);
+      });
+      currentState = undefined;
+    } catch (err) {
+      return null;
+    }
+  }
+
+  return appReducer(currentState, action);
+};
+
+export default rootReducer;
